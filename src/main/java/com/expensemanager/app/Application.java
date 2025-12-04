515902fcc4f73ffb13d759c9b5516730e1c8bfa2
LeeSomgyul <rmawl8600@naver.com>
2025-12-05 00:59:37 +0900

    기초 틀 만들기

---
+++ src/main/java/com/expensemanager/app/Application.java
@@ -0,0 +1,9 @@
+package com.expensemanager.app;
+
+public class Application {
+    public static void main(String[] args){
+        System.out.println("============================");
+        System.out.println("   개인 지출 관리 시스템");
+        System.out.println("============================");
+    }
+}
