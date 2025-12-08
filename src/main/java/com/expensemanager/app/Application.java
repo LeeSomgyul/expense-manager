dba7dc59049778540053be0464dec882d5fc4fd1
LeeSomgyul <rmawl8600@naver.com>
2025-12-08 23:13:27 +0900

    기능 추가: 1. 지출 추가하기

--- src/main/java/com/expensemanager/app/Application.java
+++ src/main/java/com/expensemanager/app/Application.java
@@ -1,9 +1,71 @@
 package com.expensemanager.app;
 
+import com.expensemanager.repository.MemoryExpenseRepository;
+import com.expensemanager.service.ExpenseService;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args){
-        System.out.println("============================");
-        System.out.println("   개인 지출 관리 시스템");
-        System.out.println("============================");
+        Scanner scanner = new Scanner(System.in);
+
+        //🔴 expenseService가  MemoryExpenseRepository만 사용하도록 의존성 주입
+        ExpenseService expenseService = new ExpenseService(new MemoryExpenseRepository());
+
+        while(true){
+            System.out.println("============================");
+            System.out.println("    개인 지출 관리 시스템");
+            System.out.println("============================");
+            System.out.println("1. 지출 추가하기");
+            System.out.println("2. 지출 목록 보기");
+            System.out.println("3. 월별 보고서 보기");
+            System.out.println("4. 삭제하기");
+            System.out.println("5. 종료하기");
+            System.out.print("메뉴 선택: ");
+
+            int choice = scanner.nextInt(); //🔴사용자가 입력 후 엔터를 누르면서 choice에 저장됨.
+            scanner.nextLine(); //🔴여기서 엔터 비워주기.
+
+            switch (choice){
+                case 1 -> addExpenseMenu(scanner, expenseService);
+                case 2 -> {}
+                case 3 -> {}
+                case 4 -> {}
+                case 5 -> {
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                }
+                default -> System.out.println("잘못된 입력입니다.");
+            }
+        }
+    }
+
+    private static void addExpenseMenu(Scanner scanner, ExpenseService expenseService){
+        System.out.println("지출 날짜를 입력하세요 (예: 2025-01-14) :");
+        System.out.print(">");
+        String dateInput = scanner.nextLine();
+
+        System.out.println("지출 내용을 입력하세요 :");
+        System.out.print(">");
+        String description = scanner.nextLine();
+
+        System.out.println("지출 금액을 입력하세요 :");
+        System.out.print(">");
+        String amountInput = scanner.nextLine();
+
+        System.out.println("카테고리를 선택하세요 :");
+        System.out.println("1. FOOD");
+        System.out.println("2. TRANSPORT");
+        System.out.println("3. SHOPPING");
+        System.out.println("4. ENTERTAINMENT");
+        System.out.println("5. ETC");
+        System.out.print(">");
+        int categoryInput = scanner.nextInt();
+        scanner.nextLine(); //🔴버퍼 비우기
+
+        //🔴ExpenseService로 사용자에게 입력받은 값들 전달
+        expenseService.addExpense(dateInput, description, amountInput, categoryInput);
+
+        System.out.println("지출이 추가되었습니다!");
     }
 }
--- src/main/java/com/expensemanager/service/ExpenseService.java
+++ src/main/java/com/expensemanager/service/ExpenseService.java
@@ -1,12 +1,68 @@
 package com.expensemanager.service;
 
+import com.expensemanager.domain.Category;
+import com.expensemanager.domain.Expense;
+import com.expensemanager.exception.InvalidInputException;
 import com.expensemanager.repository.ExpenseRepository;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class ExpenseService {
 
     private final ExpenseRepository repository;
+    private long idSequence = 1; //🔴 자동 증가 ID
 
     public ExpenseService(ExpenseRepository repository){
         this.repository = repository;
+    }
+
+    //🔴 1. 지출 추가하기
+    public void addExpense(String dateInput, String description, String amountInput, int categoryInput){
+
+        //🔴 파싱
+        LocalDate date = parseDate(dateInput); //문자 -> 날짜
+        int amount = parseAmount(amountInput); // 문자 -> 숫자
+        Category category = parseCategory(categoryInput); // 숫자 -> enum
+
+        //🔴 ID 생성
+        long id = idSequence++;
+
+        //🔴 Expense 객체 생성
+        Expense expense = new Expense(id, date, description, amount, category);
+
+        repository.save(expense);
+    }
+
+    //🔴 [추가 함수] 입력받은 문자열 데이터를 원하는 타입으로 변환하는 '파싱' 함수
+    private LocalDate parseDate(String dateInput){
+        try{
+            return LocalDate.parse(dateInput);
+        }catch (DateTimeParseException error){
+            throw new InvalidInputException("날짜 형식이 잘못되었습니다. 예: 2025-01-14");
+        }
+    }
+
+    private int parseAmount(String amountInput){
+        try{
+            int amount = Integer.parseInt(amountInput);
+            if(amount < 0){
+                throw  new InvalidInputException("금액은 음수가 될 수 없습니다.");
+            }
+            return amount;
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException("금액은 숫자로 입력해야 합니다.");
+        }
+    }
+
+    private Category parseCategory(int categoryInput){
+        return switch (categoryInput){
+            case 1 -> Category.FOOD;
+            case 2 -> Category.TRANSPORT;
+            case 3 -> Category.SHOPPING;
+            case 4 -> Category.ENTERTAINMENT;
+            case 5 -> Category.ETC;
+            default -> throw new InvalidInputException("유효하지 않은 카테고리 번호입니다.");
+        };
     }
 }
